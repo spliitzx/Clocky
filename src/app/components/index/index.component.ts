5fe3dd4f5671dcc33b86caee37a2977d3c1729f4
whenofficial <fsmitchm@gmail.com>
2019-07-29 09:40:05 +0800

    Add timer intervals

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+const moment = require('moment');
 
 @Component({
   templateUrl: './index.component.html'
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 export class IndexComponent implements OnInit {
 
   public time: string;
+  private seconds: number;
+  private timeInterval: any;
 
   // Inputs
   public timeInput: string;
+  public format: string;
 
   public saveLoading = false;
 
@@ -20,9 +24,52 @@ export class IndexComponent implements OnInit {
 
   save() {
     this.saveLoading = true;
+
+    this.seconds = moment.duration(this.timeInput).asSeconds();
+
     setTimeout(() => {
       this.saveLoading = false;
     }, 1500);
+
+    this.startTimer();
+  }
+
+  startTimer() {
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+    }
+
+    if (this.seconds) {
+      this.timeInterval = setInterval(() => {
+        this.time = this.formatTime();
+        if (this.seconds > 0) {
+          this.seconds--;
+        } else {
+          this.stopTimer();
+        }
+      }, 1000);
+    }
+  }
+
+  pauseTimer() {
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+      this.timeInterval = undefined;
+    }
+  }
+
+  stopTimer() {
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+      this.timeInterval = undefined;
+      this.time = '00:00:00';
+      this.seconds = null;
+    }
+  }
+
+  formatTime() {
+    const seconds = this.seconds;
+    return moment.utc(seconds * 1000).format('HH:mm:ss');
   }
 
 }
